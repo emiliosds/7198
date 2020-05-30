d72fcc01222b72aee55b1f1b14542e97b41b815a
stoemilio <sto.emilio@gmail.com>
2020-05-30 14:19:27 -0300

    Parte da implementação do MobX

--- lib/stores/app.store.dart
+++ lib/stores/app.store.dart
@@ -1,23 +1,31 @@
+import 'package:mobx/mobx.dart';
 import 'package:todos/models/todo-item.model.dart';
 
 class AppStore {
+  @observable
   String currentState = "none";
+  @observable
   bool busy = false;
-  List<TodoItem> todos = new List<TodoItem>();
+  @observable
+  ObservableList<TodoItem> todos = new ObservableList<TodoItem>();
 
+  @action
   void changeSelected(String state) {
     currentState = state;
   }
 
+  @action
   void add(TodoItem item) {
     todos.add(item);
   }
 
+  @action
   void setTodos(List<TodoItem> items) {
     todos.addAll(items);
   }
 
+  @action
   void clearTodos() {
-    todos = new List<TodoItem>();
+    todos = new ObservableList<TodoItem>();
   }
 }
